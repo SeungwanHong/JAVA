db1f5e8bdb860ef514f77add17bb730a294db205
SeungwanHong <swhgog@gmail.com>
2017-04-07 17:57:43 +0900

    web develop process

--- 강사님 코드/Board_20170406_2/src/kr/co/smh/dao/BaseDAO.java
+++ 강사님 코드/Board_20170406_2/src/kr/co/smh/dao/BaseDAO.java
@@ -8,10 +8,10 @@ public class BaseDAO {
 	private static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:orcl";
 	private static final String DB_USER = "smh";
 	private static final String DB_PASSWORD = "smh";
-	
+
 	protected Connection getConnection() throws SQLException {
 		Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 		return conn;
 	}
-	
+
 }
